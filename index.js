01ae901642307d7e6f724207f31f98a8af9280a7
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-05-23 20:30:24 +0600

    id gets from client side

--- index.js
+++ index.js
@@ -47,6 +47,10 @@ async function run() {
 
     })
 
+    app.delete('/addToy/:id', async(req, res)=>{
+        const id = req.params.id
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
